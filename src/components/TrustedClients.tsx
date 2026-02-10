3d4794db2f3de033424f810879775294bba4bfc1
Sanjai <sanjais.ug22.ec@scadengineering.ac.in>
2026-02-10 14:07:13 +0530

    Expand Trusted Clients list with 30+ new companies and Hari Silks

--- src/components/TrustedClients.tsx
+++ src/components/TrustedClients.tsx
@@ -8,6 +8,21 @@ interface ClientLogo {
 
 const clientLogos: ClientLogo[] = [
   {
+    name: 'AIRPORTS AUTHORITY OF INDIA',
+    logo: 'https://i.postimg.cc/5j58fQ4K/AAI.jpg',
+    alt: 'AAI Logo'
+  },
+  {
+    name: 'Larsen & Toubro Limited Construction',
+    logo: 'https://i.postimg.cc/kX45G07g/LT.png',
+    alt: 'L&T Logo'
+  },
+  {
+    name: 'Smiths Detection',
+    logo: 'https://i.postimg.cc/PNb1dvX6/SMITHS.png',
+    alt: 'Smiths Detection Logo'
+  },
+  {
     name: 'The Tamil Nadu Cricket Association',
     logo: 'https://i.postimg.cc/DmQ174vN/TNCA.png',
     alt: 'TNCA Logo'
@@ -23,15 +38,145 @@ const clientLogos: ClientLogo[] = [
     alt: 'Jet Airways Logo'
   },
   {
-    name: 'Airports Authority of India',
-    logo: 'https://i.postimg.cc/5j58fQ4K/AAI.jpg',
-    alt: 'AAI Logo'
+    name: 'TAGROS CHEMICALS INDIA PRIVATE LIMITED',
+    logo: 'https://tagros.com/wp-content/uploads/2021/08/logo.png',
+    alt: 'Tagros Logo'
   },
   {
-    name: 'Smiths Detection',
-    logo: 'https://i.postimg.cc/PNb1dvX6/SMITHS.png',
-    alt: 'Smiths Detection Logo'
+    name: 'BHUMI',
+    logo: 'https://bhumi.ngo/wp-content/uploads/2021/02/Bhumi-Logo.png',
+    alt: 'Bhumi Logo'
   },
+  {
+    name: 'TIL HEALTHCARE PRIVATE LIMITED',
+    logo: 'https://tilhealthcare.com/wp-content/uploads/2022/01/til-logo-white-1.png',
+    alt: 'TIL Healthcare Logo'
+  },
+  {
+    name: 'Hari Silks',
+    logo: 'http://harisilks.com/logo.png',
+    alt: 'Hari Silks Logo'
+  },
+  {
+    name: 'AKSS PILE FOUNDATION',
+    logo: '/logos/akss.png',
+    alt: 'AKSS Logo'
+  },
+  {
+    name: 'ARQUIVO CROP SOLUTIONS PRIVATE LIMITED',
+    logo: '/logos/arquivo.png',
+    alt: 'Arquivo Logo'
+  },
+  {
+    name: 'Bizarre Networks',
+    logo: '/logos/bizarre.png',
+    alt: 'Bizarre Networks Logo'
+  },
+  {
+    name: 'GK Shetty Builders Pvt Ltd.',
+    logo: '/logos/gkshetty.png',
+    alt: 'GK Shetty Logo'
+  },
+  {
+    name: 'Jelsie Construction',
+    logo: '/logos/jelsie.png',
+    alt: 'Jelsie Construction Logo'
+  },
+  {
+    name: 'Jencom technologies',
+    logo: '/logos/jencom.png',
+    alt: 'Jencom Logo'
+  },
+  {
+    name: 'Metronation Chennai Television Private Limited',
+    logo: '/logos/metronation.png',
+    alt: 'Metronation Logo'
+  },
+  {
+    name: 'M/s India Sports Floorings',
+    logo: '/logos/isf.png',
+    alt: 'ISF Logo'
+  },
+  {
+    name: 'MULTITECH SYSTEM INNOVATIONS INDIA PRIVATE LIMITED',
+    logo: '/logos/multitech.png',
+    alt: 'Multitech Logo'
+  },
+  {
+    name: 'MYANS VILLAS OWNERS ASSOCIATION',
+    logo: '/logos/myans.png',
+    alt: 'Myans Logo'
+  },
+  {
+    name: 'P.Ducker Systems Limited',
+    logo: '/logos/pducker.png',
+    alt: 'P.Ducker Logo'
+  },
+  {
+    name: 'Power guard system',
+    logo: '/logos/powerguard.png',
+    alt: 'Power Guard Logo'
+  },
+  {
+    name: 'Rathna Apartments',
+    logo: '/logos/rathna.png',
+    alt: 'Rathna Logo'
+  },
+  {
+    name: 'RISHAB',
+    logo: '/logos/rishab.png',
+    alt: 'RISHAB Logo'
+  },
+  {
+    name: 'Sai Lakshmi Hall',
+    logo: '/logos/sailakshmi.png',
+    alt: 'Sai Lakshmi Logo'
+  },
+  {
+    name: 'Sairam Fabrications',
+    logo: '/logos/sairam.png',
+    alt: 'Sairam Logo'
+  },
+  {
+    name: 'SL Electricals',
+    logo: '/logos/slelectricals.png',
+    alt: 'SL Electricals Logo'
+  },
+  {
+    name: 'Sri Vinayagaa Enterprises',
+    logo: '/logos/vinayagaa.png',
+    alt: 'Sri Vinayagaa Logo'
+  },
+  {
+    name: 'The Head Master',
+    logo: '/logos/headmaster.png',
+    alt: 'Head Master Logo'
+  },
+  {
+    name: 'Upspire India Private Limited',
+    logo: '/logos/upspire.png',
+    alt: 'Upspire Logo'
+  },
+  {
+    name: 'VI-SA ENTERPRISES',
+    logo: '/logos/visa.png',
+    alt: 'VI-SA Logo'
+  },
+  {
+    name: 'W & WW Technologies',
+    logo: '/logos/www.png',
+    alt: 'W&WW Logo'
+  },
+  {
+    name: 'Xports Direct Exim',
+    logo: '/logos/xports.png',
+    alt: 'Xports Logo'
+  },
+  {
+    name: 'Zytech Infra Solutions Pvt Ltd',
+    logo: '/logos/zytech.png',
+    alt: 'Zytech Logo'
+  }
 ];
 
 const TrustedClients = () => {
